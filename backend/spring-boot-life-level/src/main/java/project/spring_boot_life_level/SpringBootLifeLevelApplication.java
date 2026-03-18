826304d0680b334444bc684d95a9400961ce2e12
Martina Petrova <nekster187@gmail.com>
2026-03-18 17:57:07 +0200

    setting up

---
+++ backend/spring-boot-life-level/src/main/java/project/spring_boot_life_level/SpringBootLifeLevelApplication.java
@@ -0,0 +1,13 @@
+package project.spring_boot_life_level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLifeLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLifeLevelApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-life-level/src/test/java/project/spring_boot_life_level/SpringBootLifeLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package project.spring_boot_life_level;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLifeLevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
